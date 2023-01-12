a508475c632daa0e01a1d36d875345a2918f1f8d
Nicky <71631610+Nercules@users.noreply.github.com>
2023-01-12 21:28:40 +0100

    addud unit testing

---
+++ unit-testing/test_data_prep.py
@@ -0,0 +1,9 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import data_prep as dp
+
+
+def test_define_box_color_output():
+    input_value = [0.3, 0.5, 0.8, 0.1]
+    x = dp.define_box_color(input_value)
+    assert x == [(0, 0, 255), (0, 255, 0), (0, 255, 0) (0, 0, 255)]
