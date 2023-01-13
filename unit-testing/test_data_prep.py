a8f3d2296fceff05ce0efaa116d53e7c93c9b9d6
Nicky <71631610+Nercules@users.noreply.github.com>
2023-01-13 10:45:31 +0100

    fixed tensorflow error

--- unit-testing/test_data_prep.py
+++ unit-testing/test_data_prep.py
@@ -1,19 +1,8 @@
-# import os, sys
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-# import data_prep as dp
-
-def define_box_color(list_of_predictions):
-    box_color_list = []
-    red = (0, 0, 255)
-    green = (0, 255, 0)
-    for i in list_of_predictions:
-        if i < 0.5:
-            box_color_list.append(red)
-        else:
-            box_color_list.append(green)
-    return box_color_list
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import data_prep as dp
 
 def test_define_box_color_output():
     input_value = [0.3, 0.5, 0.8, 0.1]
-    output_value = define_box_color(input_value)
+    output_value = dp.define_box_color(input_value)
     assert output_value == [(0, 0, 255), (0, 255, 0), (0, 255, 0), (0, 0, 255)]
